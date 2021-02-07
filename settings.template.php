a0b8b9eaf2b85d5332080660a7a16c8d67aa93d5
francoe1 <francoe12012@gmail.com>
2021-02-07 14:12:31 -0300

    Update

---
+++ settings.template.php
@@ -0,0 +1,10 @@
+<?php
+
+//SSH Settings
+define("SSH_HOST", "");
+define("SSH_PORT", "");
+define("SSH_USER", "");
+define("SSH_PASS", "");
+
+define("TOKEN", "");
+define("DIR", "/var/www/vhosts/repositories/custom-project");
