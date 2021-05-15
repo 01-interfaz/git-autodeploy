35feb481b71f0f4c9c8b784518640f06fdc03208
Franco Rosatto <francoe12012@gmail.com>
2021-05-15 15:23:49 -0300

    PRueba 1

--- app/Classes/Command/CommandPayload.php
+++ app/Classes/Command/CommandPayload.php
@@ -7,7 +7,19 @@ namespace App\Classes\Command;
 class CommandPayload
 {
     public string $FolderLocation;
+
     private array $_gitBranch;
+    private array $_request;
+
+    public function SetRequest(array $request): void
+    {
+        $this->_request = $request;
+    }
+
+    public function GetRequest(): array
+    {
+        return $this->_request;
+    }
 
     public function GetGitBranchs(): array
     {
